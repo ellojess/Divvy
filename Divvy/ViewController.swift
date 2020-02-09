8b8fdf406a70648a329ee97d2bca4fb26269cee2
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-02-09 15:17:31 -0800

    Initial Commit

---
+++ Divvy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divvy
+//
+//  Created by Bo on 2/9/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
