a2b892c008a9d9d11e2e26e86185cbd778c27c0b
Henry Calderon <theceintiant@gmail.com>
2020-02-14 16:29:00 -0800

    Verification code and Confirm code view set up and files added.

---
+++ Divvy/Verification/Controllers/VerificationView.swift
@@ -0,0 +1,27 @@
+//
+//  VerificationView.swift
+//  Divvy
+//
+//  Created by Henry Calderon on 2/14/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class VerificationView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit(){
+        
+    }
+
+}
