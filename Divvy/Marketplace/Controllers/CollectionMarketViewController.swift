8a77cdad169357f54c2ee2259fceb852d7678880
Henry Calderon <theceintiant@gmail.com>
2020-03-17 13:04:15 -0700

    Need to commit right away

---
+++ Divvy/Marketplace/Controllers/CollectionMarketViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionMarketViewController.swift
+//  Divvy
+//
+//  Created by Henry Calderon on 3/17/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class CollectionMarketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Divvy/Marketplace/Views/Cells/TitleCell.swift
@@ -0,0 +1,18 @@
+//
+//  TitleCell.swift
+//  Divvy
+//
+//  Created by Henry Calderon on 3/17/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class TitleCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
