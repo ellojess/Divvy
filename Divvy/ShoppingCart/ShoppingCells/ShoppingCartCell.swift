617da914f32fc13097a7b3b259989477c1f40ae6
Henry Calderon <theceintiant@gmail.com>
2020-03-18 14:20:37 -0700

    Cell created.

---
+++ Divvy/ShoppingCart/ShoppingCells/ShoppingCartCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingCartCell.swift
+//  Divvy
+//
+//  Created by Henry Calderon on 3/18/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingCartCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
