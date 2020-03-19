a21ec758b9ccd3a08a1926876ffac80dc9c3b5d4
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-19 00:35:27 -0700

    have message cells populate in chatroom

--- Divvy/Chat/Controllers/ChatViewController.swift
+++ Divvy/Chat/Controllers/ChatViewController.swift
@@ -32,6 +32,8 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         tableView.dataSource = self
         title = K.appName
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: Any) {
@@ -57,8 +59,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
 }
